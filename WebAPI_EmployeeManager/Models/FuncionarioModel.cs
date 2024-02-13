2a50bf7c8dae2e71c424a51f9735ce082b13fd70
João Vitor Soares <jvfs12th@gmail.com>
2024-02-13 19:47:23 -0300

    :sparkles: feat: Adiciona propiedade de turno no FuncionarioModel

--- WebAPI_EmployeeManager/Models/FuncionarioModel.cs
+++ WebAPI_EmployeeManager/Models/FuncionarioModel.cs
@@ -9,6 +9,7 @@ namespace WebAPI_EmployeeManager.Models
         public string Sobrenome { get; set;}
         public DepartamentoEnum Departamento { get; set; }
         public bool Ativo { get; set; }
+        public TurnoEnum Turno { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
     }
