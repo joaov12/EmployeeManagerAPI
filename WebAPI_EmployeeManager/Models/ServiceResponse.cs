167c0d2eba82fada97a409317aded264799b12d1
João Vitor Soares <jvfs12th@gmail.com>
2024-02-13 19:51:28 -0300

    :construction_worker: build: Cria o ServiceResponse

---
+++ WebAPI_EmployeeManager/Models/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI_EmployeeManager.Models
+{
+    public class ServiceResponse<T> // Adicionando dado genério, pois no futuro posso implementar 
+                                    // um produtoModel por exemplo e usar o mesmo ServiceResponse
+    {
+        public T? Dados { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Sucesso { get; set; } = true;
+    }
+}
