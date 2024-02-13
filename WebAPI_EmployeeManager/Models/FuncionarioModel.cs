813eb18a53fa215f09e19d1d6068a942cc4bbc2c
João Vitor Soares <jvfs12th@gmail.com>
2024-02-13 19:43:49 -0300

    :recycle: refactor: Muda tipo de dado do departamento no FuncionarioModel

--- WebAPI_EmployeeManager/Models/FuncionarioModel.cs
+++ WebAPI_EmployeeManager/Models/FuncionarioModel.cs
@@ -1,11 +1,13 @@
-﻿namespace WebAPI_EmployeeManager.Models
+﻿using WebAPI_EmployeeManager.Enums;
+
+namespace WebAPI_EmployeeManager.Models
 {
     public class FuncionarioModel
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Sobrenome { get; set;}
-        public string Departamento { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
         public bool Ativo { get; set; }
         public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
         public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
