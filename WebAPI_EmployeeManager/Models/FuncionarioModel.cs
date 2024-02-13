cdb54b4f5832cf4857bb36de7c14d4b6250715aa
João Vitor Soares <jvfs12th@gmail.com>
2024-02-13 19:40:00 -0300

    :construction_worker: build: Cria o modelo Funcionário

---
+++ WebAPI_EmployeeManager/Models/FuncionarioModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI_EmployeeManager.Models
+{
+    public class FuncionarioModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set;}
+        public string Departamento { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
+    }
+}
