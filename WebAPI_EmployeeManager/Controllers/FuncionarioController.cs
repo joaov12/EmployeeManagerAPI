96d25f65afc1ea8892a60fb1037c91c53ea9eed7
João Vitor Soares <jvfs12th@gmail.com>
2024-02-14 09:43:21 -0300

    :sparkles: feat: Implementa método de Criar um funcionário

--- WebAPI_EmployeeManager/Controllers/FuncionarioController.cs
+++ WebAPI_EmployeeManager/Controllers/FuncionarioController.cs
@@ -19,5 +19,12 @@ namespace WebAPI_EmployeeManager.Controllers
         {
             return Ok(await _funcionarioInterface.GetFuncionarios());
         }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> CreateFuncionario(FuncionarioModel novoFuncionario)
+        {
+            return Ok(await _funcionarioInterface.CreateFuncionario(novoFuncionario));
+        }
+
     }
 }
